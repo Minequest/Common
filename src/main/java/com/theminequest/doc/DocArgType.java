3cd6e81bb0ca74d2af91eaa7ec69630f898c9ab6
Robert Xu <robxu9@gmail.com>
2013-10-07 18:06:40 -0400

    Move documentation package to Common.
    Signed-off-by: Robert Xu <robxu9@gmail.com>

---
+++ src/main/java/com/theminequest/doc/DocArgType.java
@@ -0,0 +1,14 @@
+package com.theminequest.doc;
+
+public enum DocArgType {
+	INT,
+	FLOAT,
+	STRING,
+	INTLOC,
+	FLOATLOC,
+	TASK,
+	EVENT,
+	REQUIREMENT,
+	TARGET,
+	IGNORE
+}
---
+++ src/main/java/com/theminequest/doc/DocDescription.java
@@ -0,0 +1,14 @@
+package com.theminequest.doc;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface DocDescription {
+	
+	String description();
+	
+}
